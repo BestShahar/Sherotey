7dcc1aaf1292c8bfb3e8bb364d20866c7e7283a9
User <User@LAB3-3>
2023-11-13 15:40:41 +0200

    Add project files.

---
+++ ConsoleApp1/HighScoresContext.cs
@@ -0,0 +1,18 @@
+﻿using Castle.Components.DictionaryAdapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public partial class HighScoresContext
+    {
+        public List<Game> AllGams()
+        {
+            return this.Games.ToList();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HighScoresContext context = new HighScoresContext();
+            foreach(Game g in context.AllGams())
+            {
+                Console.WriteLine( $"{g.Name} {g.MinimumAge}");
+            }
+        }
+    }
+}
